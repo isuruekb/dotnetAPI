27c96c7e5bec38e22a00675d995b0490125b78f5
isuruekb <isuruekb@gmail.com>
2022-09-23 15:01:22 +0530

    Add project files.

---
+++ dotnetAPI/Data/FullStackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using dotnetAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetAPI.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
